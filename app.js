77209a176ddbff4e57b9a4d8934bfb4833308af8
Chris de Villiers <cpdevill@gmail.com>
2016-02-02 16:40:47 -0500

    Basic functionality added, no good styling

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ var express = require('express');
 var morgan = require('morgan'); 
 var bodyParser = require('body-parser'); 
 var swig = require('swig');
-var $ = require("jQuery"); //maybe uppercase Q?
+var $ = require("jQuery");
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
+var app = express();
 
-var app = express(); 
+app.use(sassMiddleware({
+    src: __dirname,
+    dest: path.join(__dirname, 'public/stylesheets'),
+    debug: true,
+    outputStyle: 'compressed',
+    // prefix:  '/stylesheets'  
+}));
 
-var port = 3000; 
-
+var port = 3000;
 
 // swig set up 
 
@@ -15,7 +23,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'html'); 
 app.engine('html', swig.renderFile); 
 swig.setDefaults({ cache: false });
-
 
 //logging and bodyparsing 
 
@@ -26,7 +33,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 // routing static files 
 
-app.use(express.static(__dirname + './public'));
+app.use(express.static(__dirname + '/public'));
+
 app.use("/bootstrap", express.static('node_modules/bootstrap/dist')); 
 app.use("/jquery", express.static('node_modules/jquery/dist')); 
 
